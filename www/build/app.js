7b4a104d2825d1ccbb92ebfa9972095bba3e78c8
Nathaniel <nxm010@gmail.com>
2017-12-02 18:56:51 -0600

    com

--- www/build/app.js
+++ www/build/app.js
@@ -356,12 +356,32 @@ window.queries = [
             {
                 color: "#E47500",
                 icon: "ion-ionic",
-                title: "Info on the Art"
+                title: "Hello Ionic"
             },
-
+            {
+                color: "#5AD863",
+                icon: "ion-social-html5",
+                title: "HTML5"
+            },
+            {
+                color: "#F8E548",
+                icon: "ion-social-javascript",
+                title: "JS"
+            },
+            {
+                color: "#AD5CE9",
+                icon: "ion-social-sass",
+                title: "Sass"
+            },
+            {
+                color: "#3DBEC9",
+                icon: "ion-social-css3",
+                title: "CSS3"
+            },
+            {
                 color: "#D86B67",
                 icon: "ion-social-angular",
-                title: "About"
+                title: "Angular"
             }
         ];
 
